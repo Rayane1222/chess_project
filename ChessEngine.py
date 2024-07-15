b6703f9ca22a35e730d6c5eba9bbc656d2b74b30
Rayane BARAKAT <rayanebarakat2002@gmail.com>
2024-07-15 15:07:22 +0100

    heres what i did : -i search for images in the web and learn the name of each piece in chess -creating the files -starting with game states -creating the board dyal 8x8 2D list -and finally the blank space

--- ChessEngine.py
+++ ChessEngine.py
@@ -2,6 +2,33 @@
 here alferda we will store the information about the current game and the possible moves and stor the moves logs
 """
 
+
 class GameState():
+
+    #here a did create a sahbi board dyal 8x8 2D list and each of element of the list has 2 charactere
+    #the fist charactere f l3iba represent a color of a piece like b black w white
+    #the second is the type i just learn from internet king K queen Q rook R pawns p bishops B knights K
+    # -- is like nothing khawi empty space o sf
     def __init__(self):
-        self.board =
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+
+        self.whiteToMove = True
+        self.moveLog = []
+
+
+
+
+
+
+
+
+
+
--- ChessMain.py
+++ ChessMain.py
@@ -2,3 +2,22 @@
 here we will be the main file of the program
 """
 
+#pip install pygame
+
+import pygame as p
+import ChessEngine as ChessEngine
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8 # board is a 8x8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15 #fps pour les animention avec pygame
+IMAGES = {}
+
+'''
+here i want that the images dont lag for me like i need the images stay in the ram in the memory et pas every move i have to reload
+the images 
+
+'''
+
+def loadImages():
+
